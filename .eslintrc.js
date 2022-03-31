f96fa9631ace19328211888e03a7e14b3d9d3acf
bebeji-nappa <masadecade@gmail.com>
2022-04-01 01:43:11 +0900

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2021,
+    project: "./tsconfig.eslint.json",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+      modules: true
+    },
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["@typescript-eslint", "react", "react-hooks"],
+  root: true,
+  rules: {
+    "@typescript-eslint/no-unused-vars": "off",
+  },
+  overrides: [
+    {
+      files: ["*.tsx"],
+      rules: {
+        "react/prop-types": "off",
+      },
+    },
+  ],
+  settings: {
+    "import/resolver": {
+      node: {
+        paths: ["src"],
+      },
+    },
+  },
+};
---
+++ README.md
@@ -0,0 +1,4 @@
+# Richmd React
+<!-- ![NPM](https://img.shields.io/npm/l/react-speaker-board)
+![npm](https://img.shields.io/npm/v/react-speaker-board)
+![NPM](https://img.shields.io/npm/dw/react-speaker-board) -->
